be7171123b213637ded3a43e2e88ea9e814708e8
Synister <82519402+SynisterSage@users.noreply.github.com>
2025-09-10 14:27:37 -0400

    local changes

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,7 @@
 // NEVER commit real keys to public repos.
 export const GOOGLE_MAPS_KEY = "AIzaSyAUsXpIfhTtMcGLT0_-Yc0YLzVevLarxLk";
+export const GOLF_API_BASE_URL = "https://api.golfcourseapi.com";
+export const GOLF_COURSE_API_KEY = "UD6VS23ZHZYXDNGITWYDLJ5KXU";
+export const GEOCODER_PROVIDER: "mapbox" | "nominatim" = "nominatim";
+export const MAPBOX_ACCESS_TOKEN = "pk.eyJ1Ijoic3luYXN0ciIsImEiOiJjbWZjdGg5dTQwMXd4Mm5vcGhzYWdiZ2pjIn0.shEbawVflEEH9Aq-fOiRgQ";
+export const NOMINATIM_CONTACT = "golfapp@example.com";
